5d42d20ea2d2a5f082ac24ae7e8ccd1c1dff03c2
mellisaluce <mjlgluce@gmail.com>
2018-05-12 07:13:09 -0600

    updates tests

--- lib/key.rb
+++ lib/key.rb
@@ -1,20 +1,5 @@
 class Key
-  attr_reader :key
-
+  attr_reader
   def initialize
-    @key = {}
-    @random = []
   end
-
-  def random
-      @random << 5.times.map { rand(1..9) }.join.to_i
-  end
-  b = ["a", "b"]
-  a = [1, 2, 3]
-  h = {}
-  b.each_with_index do |num, i|
-    h[num] = [a[i], a[i+1]].join
-  end
-end
-k = Key.new
-p k.random
+end 
--- test/key_test.rb
+++ test/key_test.rb
@@ -6,9 +6,9 @@ class KeyTest < Minitest::Test
   def test_generates_5_digit_number
     new_key = Key.new
 
-    assert_equal 5, new_key.random.count
+    assert_equal 5, new_key.method.
   end
 
-  def test_can_generate
+  def test_can_generate_4_keys
   end
 end
