f0002a797b7cfafc564e692379f59e30c3c6cb6e
JP Lynch <JPLynch35@gmail.com>
2018-05-13 10:56:36 -0600

    Add extension to base character map

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,34 @@
 class Enigma
 
   def initialize
-    @character_map = {"a" => "a",
+    @base_character_map = {
+                    "A" => "A",
+                    "B" => "B",
+                    "C" => "C",
+                    "D" => "D",
+                    "E" => "E",
+                    "F" => "F",
+                    "G" => "G",
+                    "H" => "H",
+                    "I" => "I",
+                    "J" => "J",
+                    "K" => "K",
+                    "L" => "L",
+                    "M" => "M",
+                    "N" => "N",
+                    "O" => "O",
+                    "P" => "P",
+                    "Q" => "Q",
+                    "R" => "R",
+                    "S" => "S",
+                    "T" => "T",
+                    "U" => "U",
+                    "V" => "V",
+                    "W" => "W",
+                    "X" => "X",
+                    "Y" => "Y",
+                    "Z" => "Z",
+                    "a" => "a",
                     "b" => "b",
                     "c" => "c",
                     "d" => "d",
@@ -39,7 +66,28 @@ class Enigma
                     "0" => "0",
                     " " => " ",
                     "." => ".",
-                    "," => ",",}
+                    "," => ",",
+                    "!" => "!",
+                    "@" => "@",
+                    "#" => "#",
+                    "$" => "$",
+                    "%" => "%",
+                    "^" => "^",
+                    "&" => "&",
+                    "*" => "*",
+                    "(" => "(",
+                    ")" => ")",
+                    "[" => "[",
+                    "]" => "]",
+                    "<" => "<",
+                    ">" => ">",
+                    ";" => ";",
+                    ":" => ":",
+                    "/" => "/",
+                    "?" => "?",
+                    "\\" => "\\",
+                    "|" => "|"
+                     }
     @five_digit_key = Key.new
     @date_offset = Offset.new
   end
@@ -53,11 +101,12 @@ class Enigma
   end
 
   def calculate_character_map
+    # creates the encryption character map for each rotation (set current rotation equal to a_total_rotation, then b_total_rotation, etc)
     current_index = 0
     current_encryption_map = @base_character_map.transform_values do
       expected_valuetobe_index = current_index + current_rotation
-      while expected_valuetobe_index > 38
-        expected_valuetobe_index -= 39
+      while expected_valuetobe_index > 84
+        expected_valuetobe_index -= 85
       end
       current_index += 1
       @base_character_map.values[expected_valuetobe_index]
@@ -65,64 +114,36 @@ class Enigma
     current_encryption_map
   end
 
-
   def store_all_encryption_character_maps
+    # runs through all rotations (a thru d) and calls upon calculate_character_map to create the character maps, stores all 4 in an array
     @encryption_character_maps = @total_rotation_array.map do |current_rotation|
       calculate_character_map
     end
   end
 
   def encrypt(my_message, key = @five_digit_key, date = @date_offset)
-
-
+    my_message_array = my_message.chars
+    encrypted_message = []
+    my_message_array.each_with_index do |character, index|
+      if index % 4 == 0
+        encrypted_message << @encryption_character_maps[0][character]
+      elsif index % 4 == 1
+        encrypted_message << @encryption_character_maps[1][character]
+      elsif index % 4 == 2
+        encrypted_message << @encryption_character_maps[2][character]
+      elsif index % 4 == 3
+        encrypted_message << @encryption_character_maps[3][character]
+      end
+    end
+    encrypted_message.join
   end
 
-  # def calculate_b_character_map
-  #   current_rotation = @b_total_rotation
-  #   current_index = 0
-  #   current_encryption_map = @base_character_map.transform_values do
-  #     expected_valuetobe_index = current_index + current_rotation
-  #     while expected_valuetobe_index > 38
-  #       expected_valuetobe_index -= 39
-  #     end
-  #     current_index += 1
-  #     @base_character_map.values[expected_valuetobe_index]
-  #   end
-  #   current_encryption_map
-  # end
-  #
-  # def calculate_c_character_map
-  #   current_rotation = @c_total_rotation
-  #   current_index = 0
-  #   current_encryption_map = @base_character_map.transform_values do
-  #     expected_valuetobe_index = current_index + current_rotation
-  #     while expected_valuetobe_index > 38
-  #       expected_valuetobe_index -= 39
-  #     end
-  #     current_index += 1
-  #     @base_character_map.values[expected_valuetobe_index]
-  #   end
-  #   current_encryption_map
-  # end
-  #
-  # def calculate_d_character_map
-  #   current_rotation = @a_total_rotation
-  #   current_index = 0
-  #   current_encryption_map = @base_character_map.transform_values do
-  #     expected_valuetobe_index = current_index + current_rotation
-  #     while expected_valuetobe_index > 38
-  #       expected_valuetobe_index -= 39
-  #     end
-  #     current_index += 1
-  #     @base_character_map.values[expected_valuetobe_index]
-  #   end
-  #   current_encryption_map
-  # end
-
   def decrypt
+
   end
 
   def crack
+
   end
 
   def create_shift
