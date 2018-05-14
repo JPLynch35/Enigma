6cc9c32276564f848f0dec18a930822ab8ad81ba
JP Lynch <JPLynch35@gmail.com>
2018-05-14 15:29:28 -0600

    Enable user-defined encrypt key to pass through

--- lib/key.rb
+++ lib/key.rb
@@ -1,20 +1,22 @@
 class Key
-  attr_reader :key
-              :base_rotations
 
   def initialize
     @base_rotations = []
   end
 
-  def generate_key(key = 5.times.map{rand(10)})
-     @key = key.to_s
-     # key = 5.times.map { rand(1..9) }
-     #output 5d key
+  def generate_key(encrypt_key)
+    if encrypt_key = 'random'
+      key = 5.times.map{rand(10)}
+      @key = key.to_s
+    else
+      @key = encrypt_key
+    end
   end
 
-  def calculate_base_rotation(generate_key)
+  def calculate_base_rotation(encrypt_key)
+    generate_key(encrypt_key)
     4.times do |i|
-      offset = [generate_key[i], generate_key[i+1]].join
+      offset = [@key[i], @key[i+1]].join
       @base_rotations << offset
     end
     @base_rotations
