a32fb45fdfb3ee38166bb4a78604e3472839eb47
JP Lynch <JPLynch35@gmail.com>
2018-05-15 07:30:43 -0600

    Add a single crack test

--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -19,23 +19,26 @@ class EnigmaTest < Minitest::Test
   #   assert_equal [13, 47, 35, 60], @total_rotation_array
   # end
 
-  # def test_can_encrypt_word
-  #   e1 = Enigma.new
-  #   e2 = Enigma.new
-  #   e3 = Enigma.new
-  #   encrypt1 = e1.encrypt("This works!", "12345", Date.today)
-  #   encrypt2 = e2.encrypt("And this is also now encrypted!", "54321", Date.today)
-  #   encrypt3 = e3.encrypt("Or is it? It is!", "12468", Date.today)
-  #
-  #   assert_equal ">w8?l.!/L8G", encrypt1
-  #   assert_equal "XM)O$G;:BHFOxKF]BMBABDA,@XC/2Ca", encrypt2
-  #   assert_equal "T7EAx<9LD<iL#x^f", encrypt3
-  # end
+  def test_can_encrypt_word
+    e1 = Enigma.new
+    e2 = Enigma.new
+    e3 = Enigma.new
+    e4 = Enigma.new
+    encrypt1 = e1.encrypt("This works!", "12345", Date.today)
+    encrypt2 = e2.encrypt("And this is also now encrypted!", "54321", Date.today)
+    encrypt3 = e3.encrypt("Or is it? It is!", "12468", Date.today)
+    encrypt4 = e4.encrypt("Hello?", "11111")
+
+    assert_equal ">w8?l.!/L8G", encrypt1
+    assert_equal "XM)O$G;:BHFOxKF]BMBABDA,@XC/2Ca", encrypt2
+    assert_equal "T7EAx<9LD<iL#x^f", encrypt3
+    assert_equal "asy18M", encrypt4
+  end
 
   def test_can_crack_encryption
     e1 = Enigma.new
-    crack1 = e1.crack("/tD,StD|ItD$Ot.Eo>E$OsER", Date.today)
+    crack1 = e1.crack("T<]t7r]>", Date.today)
 
-    assert_equal "We are the enemy! ..end..", crack1
+    assert_equal "A..end..", crack1
   end
 end
