537974cc99f7737a71db8fd8cb59d6ed0f6380e9
JP Lynch <JPLynch35@gmail.com>
2018-05-15 15:23:55 -0600

    Add decrypt run file

---
+++ lib/decrypt.rb
@@ -0,0 +1,16 @@
+require './lib/enigma'
+
+input_variables = ARGV
+encrypted_file = input_variables[0]
+decrypted_file = input_variables[1]
+encrypt_key = input_variables[2]
+encrypt_date = input_variables[3]
+
+e = Enigma.new
+
+output = File.new("./text/" + decrypted_file, "w+")
+File.new("./text/" + encrypted_file).each_line do |line|
+  output.puts e.decrypt(line, encrypt_key, encrypt_date)
+end
+
+puts "Created 'decrypted.txt' with the key #{encrypt_key} and date #{encrypt_date}"
