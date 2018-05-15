cfb4c08ba13bf0bd2a00b358d64289d6ab78ea75
JP Lynch <JPLynch35@gmail.com>
2018-05-15 16:51:22 -0600

    Refactor generate_date_number method to work with numbered dates

--- lib/offset.rb
+++ lib/offset.rb
@@ -7,15 +7,15 @@ class Offset
   end
 
   def generate_date_number(encrypt_date)
-    if encrypt_date == 'today'
-      @date = Date.today
+    if encrypt_date == 'today' || encrypt_date == Date.today
+      date = Date.today
+      day = date.day.to_s.rjust(2, '0')
+      month = date.month.to_s.rjust(2, '0')
+      year = date.year.to_s[-2..-1]
+      @date_number = (day + month + year).to_i
     else
-      @date = encrypt_date
+      @date_number = encrypt_date.to_i
     end
-    day = @date.day.to_s.rjust(2, '0')
-    month = @date.month.to_s.rjust(2, '0')
-    year = @date.year.to_s[-2..-1]
-    @date_number = (day + month + year).to_i
   end
 
   def calculate_offset(encrypt_date)
