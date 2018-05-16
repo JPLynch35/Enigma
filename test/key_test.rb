30888868e02905a2913fd86e5da8674328faaa7b
mellisaluce <mjlgluce@gmail.com>
2018-05-16 14:14:19 -0600

    adds tests can calculate base rotations

--- test/key_test.rb
+++ test/key_test.rb
@@ -9,19 +9,19 @@ class KeyTest < Minitest::Test
     assert_instance_of Key, k
   end
 
-  def test_can_generate_key_returns_
+  def test_can_calculate_four_base_rotations
     k = Key.new
-    encrypt_key = 'random'
+    encrypt_key1 = "54321"
 
-    assert_equal String, k.generate_key(encrypt_key)
+    assert_equal 4, k.calculate_base_rotation("54321").count
   end
 
-  # def test_can_calculate_base_rotations
-  #   k = Key.new
-  #   encrypt_key = 'random'
-  #
-  #   assert_equal 0, k.calculate_base_rotation
-  # end
+  def test_base_rotations_have_two_digits
+    k = Key.new
+    base_rotation_array = k.calculate_base_rotation("54321")
+
+    assert_equal 2, base_rotation_array[0].digits.size
+  end
 
   def test_can_accept_given_key
     e1 = Enigma.new
@@ -55,31 +55,4 @@ class KeyTest < Minitest::Test
     refute_equal e2.encrypt_key, e3.encrypt_key
     refute_equal e3.encrypt_key, e1.encrypt_key
   end
-
-  # def test_can_generate_four_base_rotations
-  #   e1 = Enigma.new
-  #   e2 = Enigma.new
-  #   encrypt1 = e1.encrypt("This works!..end..")
-  #   encrypt2 = e2.encrypt("And this is also now encrypted!..end..")
-  #
-  #   assert_equal 5, e1.encrypt_key.length
-  #   assert_equal 5, e2.encrypt_key.length
-  # end
-  #
-  # def test_base_rotations_have_two_digits
-  # end
 end
-#
-# e1 = Enigma.new
-# e2 = Enigma.new
-# e3 = Enigma.new
-# e4 = Enigma.new
-# encrypt1 = e1.encrypt("This works!..end..", "12345", Date.today)
-# encrypt2 = e2.encrypt("And this is also now encrypted!..end..", "54321", Date.today)
-# encrypt3 = e3.encrypt("Or is it? It is!..end..", "10405", Date.today)
-# encrypt4 = e4.encrypt("Hello?..end..", "11111")
-#
-# assert_equal "n8^J?)]I5^Rc|5[>|F", encrypt1
-# assert_equal " B.DX;$%o:>DE|>!oB)(o](2VM[^I[PEp](3pZ", encrypt2
-# assert_equal "gyUr %]3Q%y3:pC)/^*wv^V", encrypt3
-# assert_equal "asy18M]>x2q>?", encrypt4
