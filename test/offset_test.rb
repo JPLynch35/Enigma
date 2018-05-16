85a6c213420c92a054ea0de4c8ef6ca86df652b7
mellisaluce <mjlgluce@gmail.com>
2018-05-16 14:30:50 -0600

    adds offset format test

--- test/offset_test.rb
+++ test/offset_test.rb
@@ -1,4 +1,4 @@
-require '/test/test_helper'
+require './test/test_helper'
 require './lib/offset'
 require 'pry'
 
@@ -8,30 +8,23 @@ class OffsetTest < Minitest::Test
 
     assert_instance_of Offset, o
   end
-  #
-  # def test_can_generate_date_number
-  #   o = Offset.new
-  #   o.generate_date_number
-  # end
 
-  def test_date_number_format
+  def test_generate_date_number_returns_integer
     o = Offset.new
 
-    assert_equal 6, o.date_number.count
+    assert_equal Integer, o.generate_date_number(Date.today).class
   end
-  # def test_3
-  # end
+
+  def test_date_number_six_digit_format
+    o = Offset.new
+    o.generate_date_number(Date.today)
+
+    assert_equal 6, o.date_number.to_s.length
+  end
+
+  def test_can_calculate_four_element_offset
+    o = Offset.new
+
+    assert_equal 4, o.calculate_offset(Date.today).length
+  end
 end
-# e1 = Enigma.new
-# e2 = Enigma.new
-# e3 = Enigma.new
-# e4 = Enigma.new
-# encrypt1 = e1.encrypt("This works!..end..", "12345", Date.today)
-# encrypt2 = e2.encrypt("And this is also now encrypted!..end..", "54321", Date.today)
-# encrypt3 = e3.encrypt("Or is it? It is!..end..", "10405", Date.today)
-# encrypt4 = e4.encrypt("Hello?..end..", "11111")
-#
-# assert_equal "n8^J?)]I5^Rc|5[>|F", encrypt1
-# assert_equal " B.DX;$%o:>DE|>!oB)(o](2VM[^I[PEp](3pZ", encrypt2
-# assert_equal "gyUr %]3Q%y3:pC)/^*wv^V", encrypt3
-# assert_equal "asy18M]>x2q>?", encrypt4
